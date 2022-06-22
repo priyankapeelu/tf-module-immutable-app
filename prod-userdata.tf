c4430d3ff01b78a052a0c15eb24cf19129424e23
priyankapeelu <priyankapeelu@icloud.com>
2022-06-22 16:18:29 +0530

    terraform variablescd tt

--- prod-userdata.tf
+++ prod-userdata.tf
@@ -11,11 +11,11 @@ COMPONENT=$(ls /home/roboshop/)
 GET_PASS() {
 aws secretsmanager get-secret-value --secret-id prod/roboshop/secret | jq .SecretString | sed -e 's|\\||g' -e 's|^"||' -e 's|"$||' | jq .${1} | xargs
 }
-#DOCUMENTDB_MASTER_USERNAME=$(GET_PASS DOCUMENTDB_MASTER_USERNAME)
-#DOCUMENTDB_MASTER_PASSWORD=$(GET_PASS DOCUMENTDB_MASTER_PASSWORD)
-#RABBITMQ_USERNAME=$(GET_PASS RABBITMQ_USERNAME)
-#RABBITMQ_PASSWORD=$(GET_PASS RABBITMQ_PASSWORD)
-#MONGODB_ENDPOINT=$(aws docdb describe-db-clusters --db-cluster-identifier  roboshop-prod  --output table | grep -w Endpoint | awk '{print $4}')
+DOCUMENTDB_MASTER_USERNAME=$(GET_PASS DOCUMENTDB_MASTER_USERNAME)
+DOCUMENTDB_MASTER_PASSWORD=$(GET_PASS DOCUMENTDB_MASTER_PASSWORD)
+RABBITMQ_USERNAME=$(GET_PASS RABBITMQ_USERNAME)
+RABBITMQ_PASSWORD=$(GET_PASS RABBITMQ_PASSWORD)
+MONGODB_ENDPOINT=$(aws docdb describe-db-clusters --db-cluster-identifier  roboshop-prod  --output table | grep -w Endpoint | awk '{print $4}')
 
 sed -i -e 's/ENV/prod/' -e "s/DOCUMENTDB_MASTER_USERNAME/${DOCUMENTDB_MASTER_USERNAME}/" -e "s/DOCUMENTDB_MASTER_PASSWORD/${DOCUMENTDB_MASTER_PASSWORD}/" -e "s/RABBITMQ_USERNAME/${RABBITMQ_USERNAME}/" -e "s/RABBITMQ_PASSWORD/${RABBITMQ_PASSWORD}/" -e "s/MONGODB_ENDPOINT/${MONGODB_ENDPOINT}/" /etc/systemd/system/${COMPONENT}.service /etc/filebeat/filebeat.yml
 
